2d31ebc44a937c4b714cc3fe1a2e2d823f8edb70
ExoSquiggle <iroc-z180272@Hotmail.com>
2019-03-11 18:05:28 -0700

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,10 +123,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abtesting180272@gmail.com'
-EMAIL_HOST_PASSWORD = 'pntnskwsneitbtos'
