52a1934c0ccc0ef13aad92370ac9584157e938ce
ExoSquiggle <iroc-z180272@Hotmail.com>
2019-03-11 18:04:15 -0700

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')!%rc!)6fh1s5i2+_u%(d*rwn!62f5ssiw2f30h+*dg)@3gu6a'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
